b97cf9faf1d351a9f9cc1fb9919775ea2c3f056a
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-30 08:45:48 -0700

    Pass onclick through header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '../Button/Button';
 
-const Header = () => {
+const Header = ({ selectCategory }) => {
   return (
     <div>
       <h1>Star Wars Facts</h1>
-      <Button name='favorites'/>
+      <Button name='favorites'
+              selectCategory={ selectCategory }/>
     </div>
   )
 }
