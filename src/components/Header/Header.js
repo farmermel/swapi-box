eb77a05fbf0aab586850af5799180f8cf7cf28ec
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-30 13:05:06 -0700

    Add yoda image to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Button from '../Button/Button';
+import './Header.css';
+import yoda from '../../assets/yoda.png';
 
 const Header = ({ selectCategory }) => {
   return (
     <div>
-      <h1>Star Wars Facts</h1>
+      <div>
+        <h1 className='page-title'>LEGO STAR WARS</h1>
+        <img src={ yoda } 
+             alt='lego yoda'
+             className='header-logo' />
+      </div>
       <Button name='favorites'
               selectCategory={ selectCategory }/>
     </div>
