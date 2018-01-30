424dac5675b713e51153ec0db97c1f99cd2ce319
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-30 08:45:13 -0700

    CardContainer can print array of cards using mock data

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Card from '../Card/Card';
 
-const CardContainer = () => {
+let cards;
+const createCards = data => {
+  cards = data.map( card => {
+      return <Card data={card} />
+    })
+  
+}
+
+const CardContainer = ({ cardData }) => {
+  createCards(cardData)
+//   cardData.results
+//     ? createCards(cardData)
+//     : <div></div>
   return (
     <div>
-      CardContainer
+      {cards}
     </div>
   )
 }
