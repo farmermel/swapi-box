135e1e52a7fb11571ab12916872ec2a52745a287
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-31 07:11:25 -0700

    Use link to navigate router and add images to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import './Header.css';
+import lego from '../../assets/lego.svg';
 import yoda from '../../assets/yoda.png';
 
-const Header = ({ selectCategory }) => {
+const Header = ({ getPeople, getPlanets, getVehicles, getFavs}) => {
   return (
-    <div>
-      <div>
-        <h1 className='page-title'>LEGO STAR WARS</h1>
+    <div className='header'>
+      <div className='page-header'>
+        <img src={ lego }
+             alt='lego brand'
+             className='header-logo' />
+        <h1 className='page-title'>STAR WARS</h1>
         <img src={ yoda } 
              alt='lego yoda'
              className='header-logo' />
       </div>
-      <Button name='favorites'
-              selectCategory={ selectCategory }/>
+      <nav>
+        <Link to={ {pathname: '/people'} }
+              onClick={ getPeople }>
+          <button className='people'>
+          </button>
+        </Link>
+        <Link to={ {pathname: '/planets'} }
+              onClick={ getPlanets }>
+          <button className='planets'>
+          </button>
+        </Link>
+        <Link to={ {pathname: '/vehicles'} }
+              onClick={ getVehicles }>
+          <button className='vehicles'>
+          </button>
+          </Link>
+        <Link to={ {pathname: '/favorites'} }
+              onClick={ getFavs }>
+          <button className='favorites'>
+          </button>
+        </Link>
+      </nav>
     </div>
   )
 }
