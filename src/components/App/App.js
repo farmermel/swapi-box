f17bca332b4c2bb52474c8011c990bb9add88d7f
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-31 07:05:54 -0700

    Add switch and router and get router working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Button from '../Button/Button';
 import CardContainer from '../CardContainer/CardContainer';
 import ScrollText from '../ScrollText/ScrollText';
-import api from '../../api.js';
-import mockData from '../../mock-data';
-import dataCleaner from '../../helper';
+// import api from '../../api.js';
+// import mockData from '../../mock-data';
+import swapiData from '../../helper';
 import './App.css';
 
 class App extends Component {
@@ -17,48 +18,56 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    //get scroll, title, release date
-    //randomize the film scroll in a different function
-    // const movieNum = this.randomizeFilmScroll();
-    // this.getMovie(movieNum)
-      // .then(json => this.cleanData(json));
-
-    const movieData = dataCleaner.cleanMovieData(mockData, 4)
-    // const cardData = api.getData('people')
-    const cardData = dataCleaner.cleanCardData(mockData.people.results)
-    //get people
+  async componentDidMount() {
+    const movieNum = this.randomizeFilmScroll();
+    const movieData = await swapiData.fetchMovie(movieNum)
+    const cardData = await swapiData.fetchPeople();
     this.setState({ movieData, cardData })
   }
 
-  getMovie(movieNum) {
-    console.log(api.getMovie(movieNum))
-  }
-
   randomizeFilmScroll() {
-    const lastMovieNum = 8;
+    const lastMovieNum = 7;
     return Math.ceil(Math.random() * lastMovieNum);
   }
 
-  handleSelectCategory = (category) => {
-    console.log(`clicked ${category}`)
-    //make apicall to category selected
-    //api.getData(category)
+  getPeople = () => {
+
+  }
+
+  getVehicles = () => {
+
+  }
+
+  getPlanets = () => {
+
+  }
+
+  getFavs = () => {
+
   }
 
   render() {
     return (
       <div className="App">
-        <Header selectCategory={this.handleSelectCategory}/>
-        <div>
-          <Button name='People'
-                  selectCategory={this.handleSelectCategory} />
-          <Button name='Planets'
-                  selectCategory={this.handleSelectCategory} />
-          <Button name='Vehicles'
-                  selectCategory={this.handleSelectCategory} />
-        </div>
-        <CardContainer cardData={ this.state.cardData } />
+        <Header />
+        <Switch>
+          <Route path='/people' render={ () => (
+            <CardContainer cardData={ this.state.cardData } />) }
+                           getPeople={ this.getPeople }>
+          </Route>
+          <Route path='/vehicles' render={ () => (
+            <CardContainer cardData={ this.state.cardData } />) }
+                           getVehicles={ this.getVehicles }>
+          </Route>
+          <Route path='/planets' render={ () => (
+            <CardContainer cardData={ this.state.cardData } />) }
+                           getPlanets={ this.getPlanets }>
+          </Route>
+          <Route path='/favorites' render={ () => (
+            <CardContainer cardData={ this.state.cardData } />) }
+                           getFavs={ this.getFavs }>
+          </Route>
+        </Switch>
         <ScrollText movieData={ this.state.movieData } />
       </div>
     );
