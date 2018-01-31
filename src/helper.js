1ed030bad117b0c6892ee7341b26fcda6408229c
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-31 07:13:46 -0700

    Use async await in api calls for movie data and people data. clean data to only what is needed to display

--- src/helper.js
+++ src/helper.js
@@ -1,23 +1,68 @@
- const cleanMovieData = (json, num) => {
+const root = 'https://swapi.co/api';
+
+const apiGet = async (url) => {
+  const initialFetch = await fetch(url)
+  return await initialFetch.json()
+}
+
+const cleanMovieData = (movie) => {
   return {
-    scroll: json.films.results[0].opening_crawl.toUpperCase(),
-    title: json.films.results[0].title,
-    date: json.films.results[0].release_date
+    scroll: movie.opening_crawl.toUpperCase(),
+    title: movie.title,
+    date: movie.release_date
   };
 }
 
-const cleanCardData = (json) => {
-  return json.map( item => {
-    return item = {
-      name: item.name,
-      homeworld: item.homeworld,
-      species: 'homeworld apicall'
+const fetchMovie = async (num) => {
+  const results = await apiGet(`${root}/films/${num}/`)
+  return cleanMovieData(results)
+}
+
+const cleanPeopleData = (people) => {
+  return people.map( person => {
+    return person = {
+      name: person.name,
+      homeworld: person.homeworld.name,
+      homeworldPop: person.homeworld.population,
+      species: person.species.name
     }
   })
-} 
+}
+
+const fetchPeople = async () => {
+  const { results } = await apiGet(`${root}/people/`)
+  const getPeople = await fetchPeopleDetails(results)
+  const people = await Promise.all(getPeople)
+  return cleanPeopleData(people)
+}
+
+const fetchPeopleDetails = (peopleArray) => {
+  return peopleArray.map( async (person) => {
+    let homeworld = await apiGet(person.homeworld)
+    let species = await apiGet(person.species)
+    return {...person, homeworld, species}
+  })
+
+}
+
+// async componentDidMount () {
+//   const initialFetch = await fetch('http://localhost:3001/api/frontend-staff')
+//   const { bio } = await initialFetch.json()
+//   const staff = await this.fetchBios(bio)
+//   this.setState({ staff })
+// }
+
+// fetchBios(arrayOfBios) {
+//   const unresolvedPromises = arrayOfBios.map(async (staffMember) => {
+//     let initialFetch = await fetch(staffMember.info)
+//     let bio = await initialFetch.json()
+//     return {...staffMember, ...bio}
+//   })
+//   return Promise.all(unresolvedPromises)
+// }
 
 
 export default {
-  cleanMovieData,
-  cleanCardData
+  fetchMovie,
+  fetchPeople
 }
