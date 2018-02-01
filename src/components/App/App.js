2fa505375f1410a649309d2be302df562bbabcec
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-01-31 19:26:59 -0700

    Move api calls to click handlers so only data in use is being fetched

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from '../Header/Header';
-import Button from '../Button/Button';
 import CardContainer from '../CardContainer/CardContainer';
 import ScrollText from '../ScrollText/ScrollText';
-// import mockData from '../../mock-data';
 import swapiData from '../../helpers/helper';
 import './App.css';
 
@@ -21,30 +19,30 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    const movieNum = this.randomizeFilmScroll();
-    const movieData = await swapiData.fetchMovie(movieNum);
-    // const peopleData = await swapiData.fetchPeople();
-    const planetData = await swapiData.fetchPlanets()
-    this.setState({ movieData, planetData });
-
-    // this.setState({ movieData, peopleData });
+    // const movieNum = this.randomizeFilmScroll();
+    const movieData = await swapiData.fetchMovie();
+    const peopleData = await swapiData.fetchPeople();
+    this.setState({ movieData, peopleData });
   }
 
-  randomizeFilmScroll() {
-    const lastMovieNum = 7;
-    return Math.ceil(Math.random() * lastMovieNum);
+  // getPeople = () => {
+
+  // }
+
+  getVehicles = async () => {
+    const vehicleState = this.state.vehicleData;
+    vehicleState.length === 0 
+    && this.setState({ 
+      vehicleData: await swapiData.fetchVehicles() 
+    });
   }
 
-  getPeople = () => {
-
-  }
-
-  getVehicles = () => {
-
-  }
-
-  getPlanets = () => {
-
+  getPlanets = async () => {
+    const planetState = this.state.planetData;
+    planetState.length === 0
+    && this.setState({ 
+      planetData: await swapiData.fetchPlanets() 
+    });
   }
 
   getFavs = () => {
@@ -54,26 +52,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header getVehicles={ this.getVehicles }
+                getPlanets={ this.getPlanets } />
         <Switch>
           <Route path='/people' render={ () => (
             <CardContainer cardData={ this.state.peopleData } 
-                           getPeople={ this.getPeople }
+                           getData={ this.getPeople }
                            cardType='PeopleCard' />) }>
           </Route>
           <Route path='/vehicles' render={ () => (
             <CardContainer cardData={ this.state.vehicleData } 
-                           getVehicles={ this.getVehicles }
+                           getData={ this.getVehicles }
                            cardType='VehicleCard' />) }>
           </Route>
           <Route path='/planets' render={ () => (
             <CardContainer cardData={ this.state.planetData } 
-                           getPlanets={ this.getPlanets } 
+                           getData={ this.getPlanets } 
                            cardType='PlanetCard'/>) }>
           </Route>
           <Route path='/favorites' render={ () => (
             <CardContainer cardData={ this.state.favorites } 
-                           getFavs={ this.getFavs } 
+                           getData={ this.getFavs } 
                            cardType='FavoritesCard'/>) }>
           </Route>
         </Switch>
