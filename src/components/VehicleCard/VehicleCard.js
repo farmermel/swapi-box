ba2b833f4ddf2b01bdb994d5d76cfc85af769856
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-01 07:04:03 -0700

    Create different card components for each class of information and style them to be different colors

--- src/components/VehicleCard/VehicleCard.js
+++ src/components/VehicleCard/VehicleCard.js
@@ -4,7 +4,11 @@ import './VehicleCard.css';
 const VehicleCard = ({ data }) => {
   return (
     <div className='card vehicle-card'>
-      vehicleeeeee
+      <button className='favorites'></button>
+      <h2>{data.name}</h2>
+      <p>{data.model}</p>
+      <p>{data.class}</p>
+      <p>{data.passengers}</p>
       <div className='bump-top'></div>
       <div className='bump'></div>
       <div className='bump-bottom'></div>
