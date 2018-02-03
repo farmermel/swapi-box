9633fd71095f298798de8f51da3ca48bd6b7400e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 15:06:21 -0700

    Delete unused people, vehicle, and planet card components

--- src/components/ScrollText/ScrollText.js
+++ src/components/ScrollText/ScrollText.js
@@ -4,15 +4,15 @@ import './ScrollText.css';
 const ScrollText = ({ movieData }) => {
   return (
     <div>
+      <h3 className='movie-info'>
+        {movieData.title}
+        {movieData.date}
+      </h3>
       <article className='scroll-container'>
         <div className='scroll'>
           {movieData.scroll}
         </div>
       </article>
-      <h3 className='movie-info'>
-        {movieData.title}
-        {movieData.date}
-      </h3>
     </div>
   )
 }
