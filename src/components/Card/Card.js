715ec2c0f81de951fb9d4a301a12e2b93e7f37a0
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 13:41:35 -0700

    Remove object cleaning from card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import '../Card/Card.css';
 
-
-const cleanItem = (item, value) => {
-  value = item.map( entry => <li id='res-list'>{entry.name}</li>)
-  return value;
-}
-
-const Card = ({ data, cardType, toggleFav }) => {
-  let displayItems = Object.entries(data.info).map( item => {
-    let value;
-    typeof item[1] === 'object' 
-      ? value = cleanItem(item[1], value) 
-      : null; 
-    return <p><span>{ item[0] }: </span>{ value || item[1] }</p>
+const Card = ({ data, toggleFav }) => {
+  let displayItems = Object.entries(data.info).map( item => { 
+    return <p><span>{ item[0] }: </span>{ item[1] }</p>
   })
 
   return (
