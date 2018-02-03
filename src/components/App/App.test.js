e5854ba859a32a6c8302617ceba3689feb903f49
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 11:57:41 -0700

    Write working tests for component did mount and some working tests for get methods some not yet passing

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,9 +5,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 import swapiData, { mockSwapiData } from '../../helpers/helper';
 
-jest.mock('../../helpers/helper')
-// import * as swapiData from '../../helpers/__mocks__/helper.js';
-
+jest.mock('../../helpers/helper');
 
 describe('App', () => {
   let wrapper;
@@ -24,12 +22,6 @@ describe('App', () => {
     }
   })
 
-  it('renders without crashing', () => {
-
-    // let wrapper = shallow(<BrowserRouter><App /></BrowserRouter>, {disableLifeCycleMethods: true})
-    expect(wrapper).toBeDefined();
-  });
-
   it('matches the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   })
@@ -40,21 +32,50 @@ describe('App', () => {
 
   describe('componentDidMount', () => {
     it('calls fetchMovie and fetchPeople in componentDidMount', async () => {
-      // wrapper = shallow(<App />)
-      expect(mockSwapiData.__fetchMovie()).toEqual()
+      wrapper = shallow(<App />)
+
+      await expect(swapiData.fetchMovie).toHaveBeenCalled();
+      await expect(swapiData.fetchPeople).toHaveBeenCalled();
     })
 
-    it('sets state with movie and people data', () => {
+    it('sets state with movie and people data', async () => {
+      wrapper = shallow(<App />)
+      let expectedPeople = {"favorite": false, "id": 1098234, "info": {"homeworld": "Naboo", "name": "Luke Skywalker", "species": "Human"}, "type": "peopleData"}
 
+      await expect(swapiData.fetchMovie).toHaveBeenCalled();
+      await expect(swapiData.fetchPeople).toHaveBeenCalled();
+      
+      expect(wrapper.state().movieData).toEqual({"date": "2017", "scroll": "BLAHBLAH STAR WARS", "title": "SITH"})
+      expect(wrapper.state().peopleData).toEqual(expectedPeople);
     })
   })
 
   describe('getVehicles', () => {
-    it('sets state if vehicleState doesn\'t have length', () => {
+    it('calls fetchVehicles if vehicleState doesn\'t have length', async () => {
+      expect(wrapper.state().vehicleData.length).toEqual(0)
 
+      await wrapper.instance().getVehicles();
+
+      expect(swapiData.fetchVehicles).toHaveBeenCalled()
     })
 
-    it('does not set state if vehicleState has length', () => {
+    it('sets state if vehicleState doesn\'t have length', async () => {
+      expect(wrapper.state().vehicleData.length).toEqual(0)
+
+      await wrapper.instance().getVehicles();
+      // console.log(wrapper.instance().getVehicles())
+      // expect(wrapper.setState).toHaveBeenCalled()
+      console.log(wrapper.instance().getVehicles())
+      // expect(wrapper.state().vehicleData.length).toEqual(1)
+    })
+
+    it('does not set state if vehicleState has length', async () => {
+      wrapper.instance().setState({ vehicleData: [ {some: 'fakeData'}]})
+      expect(wrapper.state().vehicleData.length).toEqual(1);
+
+      await wrapper.instance().getVehicles();
+
+      expect(swapiData.fetchVehicles).not.toHaveBeenCalled()
 
     })
   })
