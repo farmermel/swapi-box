66ad44b8152030a715b27653fee463db7d4f106f
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 11:59:28 -0700

    Fix helper mock so that it is only exporting functions

--- src/helpers/__mocks__/helper.js
+++ src/helpers/__mocks__/helper.js
@@ -1,29 +1,19 @@
 import mockData from '../../mock-data.js'
 
-const mockSwapiData = jest.fn();
-
-console.log('reaching this file?')
-
-const __fetchMovie = jest.fn().mockImplementation( () => {
+const fetchMovie = jest.fn().mockImplementation( () => {
   return mockData.cleaned.film;
 });
 
-const __fetchPeople = jest.fn().mockImplementation( () => {
+const fetchPeople = jest.fn().mockImplementation( () => {
   return mockData.cleaned.people;
 });
 
-export {
-  __fetchPeople,
-  __fetchMovie
+const fetchVehicles = jest.fn().mockImplementation( () => {
+  return mockData.cleaned.vehicle;
+})
+
+export default {
+  fetchPeople,
+  fetchMovie,
+  fetchVehicles
 }
-
-// const mock = jest.fn().mockImplementation(() => {
-//   return { 
-//     fetchMove: __fetchMovie,
-//     fetchPeople: __fetchPeople
-//    };
-// });
-
-
-
-// export default mock;
