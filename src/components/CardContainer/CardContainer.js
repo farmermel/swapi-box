1a3a51655ad90f5448783c752f6767bff19fdba8
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 13:46:43 -0700

    Remove imports of unused cards and unused props

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import Card from '../Card/Card'
-// import PeopleCard from '../PeopleCard/PeopleCard';
-// import PlanetCard from '../PlanetCard/PlanetCard';
-// import VehicleCard from '../VehicleCard/VehicleCard';
-// import FavoritesCard from '../FavoritesCard/FavoritesCard';
 import loading from '../../assets/loading.gif';
 import './CardContainer.css';
 
-const createCards = ( data, cardType, toggleFav ) => {
+const createCards = ( data, toggleFav ) => {
   return data.map( (card) => {
     return <Card data={card} 
-                 cardType={cardType}
                  toggleFav={toggleFav}
                  key={card.id} />
   })
 }
 
 
-const CardContainer = ({ cardData, getData, cardType, toggleFav }) => {
+const CardContainer = ({ cardData, getData, toggleFav }) => {
   return (
     <div className='card-container'>
       {!cardData.length && <img src={loading} 
                                 alt='page is loading'
                                 className='loading-gif' /> }
-      {createCards(cardData, cardType, toggleFav)}
+      {createCards(cardData, toggleFav)}
     </div>
   )
 }
