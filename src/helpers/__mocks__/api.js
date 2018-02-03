33dfcb1d066e8284d272d0e57095d9bc54d34ee9
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-02 20:04:16 -0700

    Add mocks for helpers

---
+++ src/helpers/__mocks__/api.js
@@ -0,0 +1,13 @@
+import mockData from '../mock-data.js';
+
+
+window.fetch = jest.fn().mockImplementation( (url) => {
+  return Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve({})
+  })
+})
+
+export const mockApiGet = jest.fn().mockImplementation( (url) => {
+  return window.fetch;
+})
