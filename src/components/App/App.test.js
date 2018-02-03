6d966f2db31bbd1dc543e6b0e0712ade7fdd6b5b
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-03 10:15:28 -0700

    Add outline for testing in App and attempt to import mock helper file

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import swapiData, { mockSwapiData } from '../../helpers/helper';
 
-it('renders without crashing', () => {
-  let wrapper = shallow(<App />, {disableLifecycleMethods: true})
+jest.mock('../../helpers/helper')
+// import * as swapiData from '../../helpers/__mocks__/helper.js';
 
-  // let wrapper = shallow(<BrowserRouter><App /></BrowserRouter>, {disableLifeCycleMethods: true})
-  // const div = document.createElement('div');
-  expect(wrapper).toBeDefined();
-  // ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App', () => {
+  let wrapper;
+  let defaultState;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />, {disableLifecycleMethods: true})
+    defaultState = {
+      movieData: {},
+      peopleData: [],
+      vehicleData: [],
+      planetData: [],
+      favorites: []
+    }
+  })
+
+  it('renders without crashing', () => {
+
+    // let wrapper = shallow(<BrowserRouter><App /></BrowserRouter>, {disableLifeCycleMethods: true})
+    expect(wrapper).toBeDefined();
+  });
+
+  it('matches the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('has default state', () => {
+    expect(wrapper.state()).toEqual(defaultState);
+  })
+
+  describe('componentDidMount', () => {
+    it('calls fetchMovie and fetchPeople in componentDidMount', async () => {
+      // wrapper = shallow(<App />)
+      expect(mockSwapiData.__fetchMovie()).toEqual()
+    })
+
+    it('sets state with movie and people data', () => {
+
+    })
+  })
+
+  describe('getVehicles', () => {
+    it('sets state if vehicleState doesn\'t have length', () => {
+
+    })
+
+    it('does not set state if vehicleState has length', () => {
+
+    })
+  })
+
+  describe('getPlanets', () => {
+    it('sets state if planetState doesn\'t have length', () => {
+
+    })
+
+    it('does not set state if planetState has length', () => {
+
+    })
+  })
+
+  describe('toggleFav', () => {
+
+
+
+
+  })
+
+
+})
